a94c6f7eef3a529a0185e269d5d022eb98ac11c4
Fernando <fernandovillab@gmail.com>
2016-09-20 09:39:54 +0200

    Changed favicon to comply with license termns.

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
     <div class="row text-center">
       <h3>&copy; <?php echo date("Y"); ?> Applied Web Architecture - Jönköping University</h3>
       <h5>Definitons taken from <a href="https://en.wikipedia.org/wiki/Main_Page" target="_blank">Wikipedia</a></h5>
+      <div>Favicon made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     </div>
   </footer>
 
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="shortcut icon" type="image/x-icon" href="books-icon.ico">
+  <link rel="shortcut icon" type="image/svg+xml" href="3d-dictionary.svg">
 
   <title>Definition App</title>
 
